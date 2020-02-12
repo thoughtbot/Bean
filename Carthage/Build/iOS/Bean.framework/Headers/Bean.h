a2ea5ed071442059613ee44bc4b7be0e6cc2b930
amangona <abe@thoughtbot.com>
2020-02-12 10:45:33 -0600

    carthage support

---
+++ Carthage/Build/iOS/Bean.framework/Headers/Bean.h
@@ -0,0 +1,19 @@
+//
+//  Bean.h
+//  Bean
+//
+//  Created by abe on 2/11/20.
+//  Copyright © 2020 thoughtbot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bean.
+FOUNDATION_EXPORT double BeanVersionNumber;
+
+//! Project version string for Bean.
+FOUNDATION_EXPORT const unsigned char BeanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bean/PublicHeader.h>
+
+
