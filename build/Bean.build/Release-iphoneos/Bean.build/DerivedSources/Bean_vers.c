da1128f39f2dcbc071210aac9e9230a581210b16
amangona <abe@thoughtbot.com>
2020-02-12 13:49:04 -0600

    removed build folder

--- build/Bean.build/Release-iphoneos/Bean.build/DerivedSources/Bean_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char BeanVersionString[];
- extern const double BeanVersionNumber;
-
- const unsigned char BeanVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Bean  PROJECT:Bean-1" "\n";
- const double BeanVersionNumber __attribute__ ((used)) = (double)1.;
--- build/Bean.build/Release-iphoneos/Bean.build/Objects-normal/arm64/Bean_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/abe/Documents/Bean/build/Bean.build/Release-iphoneos/Bean.build/DerivedSources/Bean_vers.c
